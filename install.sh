95576e71956a0eda3818b3855d61fe9d8721274b
Shivang Chikani <34369171+schikani@users.noreply.github.com>
2020-12-27 20:11:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ mkdir ~/bin
 cp fileSorter.py ~/bin/fileSorter
 sudo echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc
 source ~/.bashrc
-echo -e "Installation complete.\nNow you can simply type '"fileSorter path/to/directory"' to use fileSorter.\n\nNote: Don't execute this script more than once! \n"
+echo -e "\nInstallation complete.\nNow you can simply type '"fileSorter path/to/directory"' to use fileSorter.\n\nNote: Don't execute this script more than once! \n"
