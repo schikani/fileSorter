182d06de3fa9f44e02aa42f7823c19266eb0b93d
schikani <schikani>
2021-01-15 23:37:29 +0100

    Update fileSorter.py and add database.json

--- fileSorter.py
+++ fileSorter.py
@@ -3,43 +3,100 @@
 import os
 import shutil
 from sys import argv
+import json
 
 
-def folderName(name):
-    i = name.rfind(".") + 1
-    if i == 0:
+class FileSorter:
+
+    def __init__(self):
+        self.initialize()
+
+    def initialize(self):
+        for arg in argv[1:]:
+            if arg == ".":
+                arg = str(os.getcwd())
+            if arg[-1] == "/":
+                arg = arg[:-1]
+            self.fileSorter(arg)
+
+    @staticmethod
+    def jsonRead():
+        database = open("database.json", "r")
+        j_read = json.load(database)
+        database.close()
+        return j_read
+
+    @staticmethod
+    def jsonWrite(_list, folder_name):
+        _list.append(folder_name)
+        _dictionary = {"Other": _list}
+        with open("Other.json", "w") as write:
+            json.dump(_dictionary, write)
+
+    def folderName(self, name):
+        found = False
         folder_name = name
-    else:
-        folder_name = name[i:]
+        key_list = list(self.jsonRead()["type"].keys())
 
-    return folder_name
+        for t in key_list:
+            val_list = list(self.jsonRead()["type"][t])
+            if folder_name in val_list:
+                index = key_list.index(t)
+                folder_name = key_list[index]
+                found = True
+                break
 
+        _list = list()
 
-def fileSorter(folder):
-    if os.path.exists(folder):
-        sorted_folder = folder + "_sorted"
-        os.mkdir(sorted_folder)
+        if not found:
+            try:
+                with open("Other.json", "r") as _read:
+                    _r = json.load(_read)
+                    for item in _r["Other"]:
+                        _list.append(item)
 
-        for root, dir_, files in os.walk(folder):
-            for f in files:
-                try:
-                    os.mkdir(os.path.join(sorted_folder, folderName(f)))
-                    shutil.move(os.path.join(root, f), os.path.join(sorted_folder, folderName(f)))
-                except FileExistsError:
-                    shutil.move(os.path.join(root, f), os.path.join(sorted_folder, folderName(f)))
+                self.jsonWrite(_list, folder_name)
 
-        shutil.rmtree(folder)
-        os.rename(sorted_folder, folder)
-        print(f"'{folder}' is sorted!")
+            except FileNotFoundError:
+                self.jsonWrite(_list, folder_name)
 
-    else:
-        print(f"'{folder}' does not exist!")
+            except json.decoder.JSONDecodeError:
+                self.jsonWrite(_list, folder_name)
+
+            folder_name = "Other"
+
+        return folder_name
+
+    def fileSorter(self, folder):
+        if os.path.exists(folder):
+            sorted_folder = folder + "_sorted"
+            os.mkdir(sorted_folder)
+            for root, dir_, files in os.walk(folder):
+                for f in files:
+
+                    i = f.rfind(".") + 1
+
+                    if i == 0:
+                        folder_name = f
+
+                    else:
+                        folder_name = f[i:]
+
+                    try:
+                        folder_name = self.folderName(folder_name)
+                        os.mkdir(os.path.join(sorted_folder, folder_name))
+                        shutil.move(os.path.join(root, f), os.path.join(sorted_folder, folder_name))
+
+                    except FileExistsError:
+                        shutil.move(os.path.join(root, f), os.path.join(sorted_folder, folder_name))
+
+            shutil.rmtree(folder)
+            os.rename(sorted_folder, folder)
+            print(f"'{folder}' is sorted!")
+
+        else:
+            print(f"'{folder}' does not exist!")
 
 
 if __name__ == "__main__":
-    for arg in argv[1:]:
-        if arg == ".":
-            arg = str(os.getcwd())
-        if arg[-1] == "/":
-            arg = arg[:-1]
-        fileSorter(arg)
+    FileSorter()
